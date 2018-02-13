237bee3534c13ac1a5fece1b7b49cb53b978dcaf
Daniela Munoz <daniela.munoz@enova.mx>
2018-02-13 12:21:44 -0600

    Adding styles place detail view

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -4,18 +4,19 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'app-detalle',
-	templateUrl: './detalle.component.html'
+	templateUrl: './detalle.component.html',
+	styleUrls: ['./detalle.component.scss']
 })
 
 
 export class DetalleComponent {
 	lugares: any = [
-		{ id: 1, plan: 'gratuito', cercania: 1, distancia: 1, active: true, nombre: 'Florería la gardenia', description: 'Descripción de este negocio' },
-		{ id: 2, plan: 'pagado', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita', description: 'Descripción de este negocio' },
-		{ id: 3, plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria huellitas', description: 'Descripción de este negocio' },
-		{ id: 4, plan: 'gratuito', cercania: 2, distancia: 10, active: false, nombre: 'Florería la gardenia II', description: 'Descripción de este negocio' },
-		{ id: 5, plan: 'gratuito', cercania: 2, distancia: 35, active: true, nombre: 'Zapatería classic', description: 'Descripción de este negocio' },
-		{ id: 6, plan: 'pagado', cercania: 3, distancia: 125, active: true, nombre: 'Suhi Roll', description: 'Descripción de este negocio' }
+		{ id: 1, plan: 'gratuito', cercania: 1, distancia: 1, active: true, nombre: 'Florería la gardenia', img: '../../assets/img/tienda-floreria-gardenias.png' },
+		{ id: 2, plan: 'pagado', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita', img:'../../assets/img/tienda-donas-pasadita.png' },
+		{ id: 3, plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria huellitas', img:'../../assets/img/tienda-accesorios-huellitas.png'},
+		{ id: 4, plan: 'gratuito', cercania: 2, distancia: 10, active: false, nombre: 'Florería la gardenia II', img: '../../assets/img/tienda-floreria-gardenias.png'},
+		{ id: 5, plan: 'gratuito', cercania: 2, distancia: 35, active: true, nombre: 'Zapatería classic', img: '../../assets/img/tienda-zapateria-artesanos.png' },
+		{ id: 6, plan: 'pagado', cercania: 3, distancia: 125, active: true, nombre: 'Suhi Roll', img:'../../assets/img/tienda-estudio-corte-laser.png' }
 	];
 	id = null;
 	lugar: any = [];
